25da176370da0f080b20704f97bae50327649c9c
dyxgou <diego5614yt@gmail.com>
2025-10-28 09:06:13 -0500

    add(grade): Create an index on the grades table that checks if the grades of a subject already has a final exam grade

--- migrations/00004_grade.sql
+++ migrations/00004_grade.sql
@@ -8,6 +8,28 @@ CREATE TABLE IF NOT EXISTS grade (
   FOREIGN KEY (subject_id) REFERENCES subject (id)
 );
 
+-- Performance optimization: Add index on subject_id
+CREATE INDEX IF NOT EXISTS idx_grade_subject_id ON grade (subject_id);
+
+CREATE TRIGGER IF NOT EXISTS trg_check_final_grade_requirement BEFORE INSERT ON grade FOR EACH ROW WHEN NEW.is_final_exam = FALSE BEGIN
+SELECT
+  RAISE (
+    FAIL,
+    'Cannot add non-final grade: subject has reached maximum grades (9). Next grade must be final.'
+  )
+WHERE
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      grade
+    WHERE
+      subject_id = NEW.subject_id
+      AND is_final_exam = FALSE
+  ) = 9;
+
+END;
+
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
